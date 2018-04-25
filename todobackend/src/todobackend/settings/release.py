ea156f2a629068cc48396ae032c7be68c4d68734
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2018-04-25 22:26:22 +0800

    add release stage

--- todobackend/src/todobackend/settings/release.py
+++ todobackend/src/todobackend/settings/release.py
@@ -20,5 +20,5 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = os.environ.get("STATIC_ROOT", '/var/www/todobackend/static')
+STATIC_ROOT = os.environ.get('STATIC_ROOT', '/var/www/todobackend/static')
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT", '/var/www/todobackend/media')
