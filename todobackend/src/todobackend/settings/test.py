edad3afa4ddd28860e9d2b194d60de4cdfa18931
张明凯 <zhangmingkai@cnnic.cn>
2018-04-26 16:54:56 +0800

    down make file with v2 docker-compose file

--- todobackend/src/todobackend/settings/test.py
+++ todobackend/src/todobackend/settings/test.py
@@ -4,7 +4,7 @@ import os
 INSTALLED_APPS += ('django_nose',)
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-TEST_OUT_DIR = os.environ.get('TEST_OUT_DIR', '.')
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
 NOSE_ARGS = [
   '--verbosity=2',
   '--nologcapture',
@@ -13,9 +13,9 @@ NOSE_ARGS = [
   '--with-spec',
   '--spec-color',
   '--with-xunit',
-  '--xunit-file=%s/unittests.xml' % TEST_OUT_DIR,
+  '--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
   '--cover-xml',
-  '--cover-xml-file=%s/coverage.xml' % TEST_OUT_DIR,
+  '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
 ]
 
 DATABASES = {
